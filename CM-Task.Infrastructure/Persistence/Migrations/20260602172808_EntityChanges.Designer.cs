b8e50562a20bef4ee7a885a1cf91173b8d05cdab
Bart <filipbart1@gmail.com>
2026-06-02 19:51:45 +0200

    feat(infrastructure): update EF Core model configuration and add migration files

--- CM-Task.Infrastructure/Persistence/Migrations/20260602172808_EntityChanges.Designer.cs
+++ CM-Task.Infrastructure/Persistence/Migrations/20260602172808_EntityChanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CM_Task.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CM_Task.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260602172808_EntityChanges")]
+    partial class EntityChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.8");
